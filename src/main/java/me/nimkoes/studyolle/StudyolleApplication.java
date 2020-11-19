9c010ca9663bbd3170add88be1023c45d8eb0b85
xxxelppa <xxxelppa@gmail.com>
2020-11-19 23:35:05 +0900

    Initial commit

---
+++ src/main/java/me/nimkoes/studyolle/StudyolleApplication.java
@@ -0,0 +1,13 @@
+package me.nimkoes.studyolle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyolleApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(StudyolleApplication.class, args);
+    }
+    
+}
