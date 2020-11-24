6fc0bbee54f866fc7eef9f28834895f8afaa0fa4
xxxelppa <xxxelppa@gmail.com>
2020-11-24 22:49:17 +0900

    [12/96] 회원가입 :: 회원 가입 폼 서브밋 검증

--- src/main/java/me/nimkoes/studyolle/account/AccountController.java
+++ src/main/java/me/nimkoes/studyolle/account/AccountController.java
@@ -1,12 +1,36 @@
 package me.nimkoes.studyolle.account;
 
+import com.fasterxml.jackson.databind.jsontype.impl.SubTypeValidator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
-
+    
+    private final SignUpFormValidator signUpFormValidator;
+    
+    /*
+     * validator 를 추가 해놓으면
+     * signUpForm 을 받을 때, @InitBinder("signUpForm") 여기에 작성한 signUpForm 이것과
+     * public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) { 여기의 SignUpForm 이 매칭되어
+     * validation 을 수행한다.
+     *
+     *  -> signUpSubmit 안에서 추가로 작성해줄 필요가 없다.
+     */
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
+    
     @GetMapping("/sign-up")
     public String signUpForm(Model model) {
 //        model.addAttribute("signUpForm", new SignUpForm());
@@ -17,5 +41,18 @@ public class AccountController {
          */
         model.addAttribute(new SignUpForm());
         return "account/sign-up";
+    }
+    
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+        /*
+         * SignUpForm 의 Validation 이 실패하면 그 정보가 errors 에 담긴다.
+         */
+        if (errors.hasErrors()) {
+            return "account/sign-up";
+        }
+        
+        // TODO 회원 가입 처리
+        return "redirect:/";
     }
 }
---
+++ src/main/java/me/nimkoes/studyolle/account/AccountRepository.java
@@ -0,0 +1,11 @@
+package me.nimkoes.studyolle.account;
+
+import me.nimkoes.studyolle.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
+}
